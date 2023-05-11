6d5464272ca445b0802decf699f95409932bd049
MarikaGK <113508344+MarikaGK@users.noreply.github.com>
2023-05-11 09:44:12 +0200

    add statistics with title

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -25,6 +25,7 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
+      <Statistics title="Upload stats" stats={data} />
       <Statistics stats={data} />
       <FriendsList friends={friends} />
       <TransactionHistory items={transactions} />;
